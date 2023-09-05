d1abb832636f1c7fe35fd3ddbf1e82276e70ae2f
Carlos Ali Schutz <carlos.schutz@cwi.com.br>
2023-09-05 10:38:50 -0300

    ajustes

--- assets/constants/projetos.js
+++ assets/constants/projetos.js
@@ -1,52 +1,52 @@
 export const PROJETOS = [
   {
     titulo: "Taskers",
-    imagem: "/assets/img/Taskers.jpg",
+    imagem: "./assets/img/Taskers.jpg",
     github: "https://github.com/CarlosAliSchutz/Taskers",
   },
   {
     titulo: "Rede Social .NET",
-    imagem: "/assets/img/Rede Social .NET.jpg",
+    imagem: "./assets/img/Rede Social .NET.jpg",
     github: "https://github.com/CarlosAliSchutz/RedeSocial",
   },
   {
     titulo: "Currículo Pessoal",
-    imagem: "/assets/img/Curriculo.jpg",
+    imagem: "./assets/img/Curriculo.jpg",
     github: "https://github.com/CarlosAliSchutz/Curriculo-pessoal",
   },
   {
     titulo: "Wizzi Checkout Viagens",
-    imagem: "/assets/img/Wizzi.jpg",
+    imagem: "./assets/img/Wizzi.jpg",
     github: "https://github.com/CarlosAliSchutz/Wizzi",
   },
   {
     titulo: "Gerador de senha",
-    imagem: "/assets/img/Gerador-Senha.jpg",
+    imagem: "./assets/img/Gerador-Senha.jpg",
     github: "https://github.com/CarlosAliSchutz/Gerador-senha",
   },
   {
     titulo: "API GitHub",
-    imagem: "/assets/img/GitHub-api.jpg",
+    imagem: "./assets/img/GitHub-api.jpg",
     github: "https://github.com/CarlosAliSchutz/API-Github",
   },
   {
     titulo: "Formulário com Validação",
-    imagem: "/assets/img/Formulario-Validacao.jpg",
+    imagem: "./assets/img/Formulario-Validacao.jpg",
     github: "https://github.com/CarlosAliSchutz/Formulario-Validacao",
   },
   {
     titulo: "Menu Dragon Ball",
-    imagem: "/assets/img/Dragon-Ball.jpg",
+    imagem: "./assets/img/Dragon-Ball.jpg",
     github: "https://github.com/CarlosAliSchutz/Dragon-Ball2.0",
   },
   {
     titulo: "Acordeon",
-    imagem: "/assets/img/Acordeon.jpg",
+    imagem: "./assets/img/Acordeon.jpg",
     github: "https://github.com/CarlosAliSchutz/Acordeon",
   },
   {
     titulo: "Simples Landing Page",
-    imagem: "/assets/img/Simples-Landing-Page.jpg",
+    imagem: "./assets/img/Simples-Landing-Page.jpg",
     github: "https://github.com/CarlosAliSchutz/Landing-Page-Grid",
   },
 ];
